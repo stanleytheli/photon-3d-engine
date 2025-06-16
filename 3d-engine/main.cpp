99ace298b3029f83ee33c3088e4037937766058d
stanley <stanleytheli@gmail.com>
2025-06-15 20:10:00 -0700

    Update shaders

--- 3d-engine/main.cpp
+++ 3d-engine/main.cpp
@@ -5,11 +5,14 @@
 #include "src/graphics/buffers/buffer.h"
 #include "src/graphics/buffers/indexbuffer.h"
 #include "src/graphics/buffers/vertexarray.h"
+#include "src/graphics/simplerenderer2d.h"
+#include "src/graphics/renderable2d.h"
 
 #include "src/math/math.h"
 #include "src/utils/fileUtils.h"
 
 #define PRINT(x) std::cout << x << std::endl
+#define PI 3.141592653
 
 int main() {
 	using namespace photon::graphics;
@@ -18,33 +21,42 @@ int main() {
 	Window window("Renderer", 1000, 700);
 
 	glClearColor(0.454, 0.890, 0.882, 1.0);
-
+	
+	// matrix and vector testing
 	/*
-	GLfloat vertices[] = {
-	-0.5, -0.5, 0.0,
-	 0.5, -0.5, 0.0,
-	 0.0,  0.5, 0.0,
-	};
+	mat4x4 Rx = mat4x4::rotate(vec3(1, 0, 0), PI / 3);
+	mat4x4 Ry = mat4x4::rotate(vec3(0, 1, 0), PI / 3);
+	mat4x4 Rz = mat4x4::rotate(vec3(0, 0, 1), PI / 3);
 
-	GLuint vbo;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
+	PRINT(Rx);
+	PRINT(Ry);
+	PRINT(Rz);
 	*/
 
+	// Old rendering pipeline 
+	/*
 	GLfloat vertices[] = {
-		-0.5, -0.5, 0.0,
-		 0.5, -0.5, 0.0,
-		 0.0,  0.5, 0.0,
-		 0.3,  0.25,0.0,
-		 0.9,  0.25,0.0,
-		 0.7, -0.25,0.0,
-		-0.3,  0.25,0.0,
-		-0.9,  0.25,0.0,
-		-0.7, -0.25,0.0,
+		-0.5, -0.5,  0.0,
+		 0.5, -0.5,  0.0,
+		 0.0,  0.5,  0.0,
+		 0.3,  0.25, 0.0,
+		 0.9,  0.25, 0.0,
+		 0.7, -0.25, 0.0,
+		-0.3,  0.25, 0.0,
+		-0.9,  0.25, 0.0,
+		-0.7, -0.25, 0.0,
+	};
+
+	GLfloat colors[] = {
+		 0.219, 0.584, 0.878, 1.0,
+		 0.922, 0.290, 0.259, 1.0,
+		 0.125, 0.722, 0.035, 1.0,
+		 0.219, 0.584, 0.878, 1.0,
+		 0.922, 0.290, 0.259, 1.0,
+		 0.125, 0.722, 0.035, 1.0,
+		 0.219, 0.584, 0.878, 1.0,
+		 0.922, 0.290, 0.259, 1.0,
+		 0.125, 0.722, 0.035, 1.0,
 	};
 
 	GLuint indices[] = {
@@ -52,36 +64,34 @@ int main() {
 		3, 4, 5,
 		6, 7, 8,
 	};
-
-	VertexArray vao;
-	Buffer* vbo = new Buffer(vertices, 9 * 3, 3);
-	IndexBuffer* ibo = new IndexBuffer(indices, 9);
-
-	vao.addBuffer(vbo, 0);
+	*/
 
 	Shader shader("src/shaders/basic.vert", "src/shaders/basic.frag");
 	shader.enable();
 
-	shader.setUniformMat4("pr_matrix", mat4x4::scale(1.0));
+	shader.setUniformMat4("pr_matrix", mat4x4::identity());
 	shader.setUniformMat4("vw_matrix", mat4x4::identity());
-	shader.setUniformMat4("ml_matrix", mat4x4::identity());
 
 	shader.setUniform2f("light_pos", vec2(0.0, 0.0));
+
+	Renderable2D sprite1(vec3(0.2, 0.2, 0), vec2(0.5, 0.5), vec4(0.922, 0.290, 0.259, 1.0), shader);
+	Renderable2D sprite2(vec3(-0.2, 0, 0), vec2(0.3, 0.7), vec4(0.219, 0.584, 0.878, 1.0), shader);
+	Renderable2D sprite3(vec3(-0.1, -0.5, 0), vec2(0.4, 0.6), vec4(0.125, 0.722, 0.035, 1.0), shader);
+	SimpleRenderer2D renderer;
 
 	while (!window.closed()) {
 		window.clear();
 
-		float x = (float)window.getMouseX(), y = (float)window.getMouseY();
+		float x = window.getMouseX(), y = window.getMouseY();
 		float w = window.getWidth(), h = window.getHeight();
 		float normMouseX = 2 * (x/w) - 1;
 		float normMouseY = 1 - 2 * (y/h);
 		shader.setUniform2f("light_pos", vec2(normMouseX, normMouseY));
 
-		vao.bind();
-		ibo->bind();
-		glDrawElements(GL_TRIANGLES, ibo->getCount(), GL_UNSIGNED_INT, 0);
-		vao.unbind();
-		ibo->unbind();
+		renderer.submit(&sprite1);
+		renderer.submit(&sprite2);
+		renderer.submit(&sprite3);
+		renderer.flush();
 
 		window.update();
 	}
--- 3d-engine/src/shaders/basic.frag
+++ 3d-engine/src/shaders/basic.frag
@@ -1,13 +1,21 @@
 #version 330 core
 
-layout (location = 0) out vec4 fragColor;
+layout (location = 0) out vec4 color;
+in DATA {
+	vec4 position;
+	vec4 color;
+} fs_in;
 
-in vec4 pos;
+
 uniform vec2 light_pos = vec2(0.0, 0.0);
-uniform float light_str = 0.5;
+
+uniform vec4 lightColor = vec4(1.0, 1.0, 1.0, 1.0);
+uniform float light_str = 0.1;
 
 void main() {
-	float L = length(pos.xy - light_pos);
-	float intensity = light_str / sqrt(L);
-	fragColor = vec4(0.871, 0.235, 0.298, 1.0) * intensity;
+	color = fs_in.color;
+
+	float L = length(fs_in.position.xy - light_pos);
+	float intensity = 1 / sqrt(L);
+	color += light_str * lightColor * intensity;
 }

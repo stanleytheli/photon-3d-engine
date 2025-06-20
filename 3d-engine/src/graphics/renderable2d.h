5aabb6420b5b74d5a8cc30bb3e7049102da3b26c
stanley <stanleytheli@gmail.com>
2025-06-20 01:48:20 -0700

    Make destructor public

--- 3d-engine/src/graphics/renderable2d.h
+++ 3d-engine/src/graphics/renderable2d.h
@@ -20,13 +20,12 @@ namespace photon {
 			math::vec3 m_position;
 			math::vec2 m_size;
 			math::vec4 m_color;
-
-			Renderable2D(math::vec3 position, math::vec2 size, math::vec4 color) 
-				: m_position(position), m_size(size), m_color(color) {}
-
-			virtual ~Renderable2D() {}
+			Renderable2D(math::vec3 position, math::vec2 size, math::vec4 color)
+				: m_position(position), m_size(size), m_color(color) {
+			}
 
 		public:
+			virtual ~Renderable2D() {}
 			inline const math::vec3& getPosition() const { return m_position; }
 			inline const math::vec2& getSize() const { return m_size; }
 			inline const math::vec4& getColor() const { return m_color; }
